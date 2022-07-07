a1f0ef58f94981b6480f57cbae6d01719062c55e
jack-barr3tt <jjbdev21@gmail.com>
2022-07-07 18:51:21 +0100

    Initialize backend

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+@Module({
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: +(process.env.POSTGRES_PORT || ''),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            autoLoadEntities: true,
+            synchronize: true,
+        }),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: 'schema.gql',
+        }),
+    ]
+})
+export class AppModule {}
