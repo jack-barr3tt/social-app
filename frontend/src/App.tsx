f10efcf859cf43874fde8df4e573480db5e24432
jack-barr3tt <jjbdev21@gmail.com>
2022-07-08 20:02:34 +0100

    Set up apollo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
+
+const client = new ApolloClient({
+    uri: "http://localhost:3000/graphql",
+    cache: new InMemoryCache()
+})
+
 function App() {
-	return <></>
+	return <ApolloProvider client={client}>
+    </ApolloProvider>
 }
 
 export default App
