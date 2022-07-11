c8f85e293a8a7babc5c5db3757bc697a28258f4f
jack-barr3tt <jjbdev21@gmail.com>
2022-07-11 15:54:35 +0100

    Improved title component

--- frontend/src/Components/Title.tsx
+++ frontend/src/Components/Title.tsx
@@ -1,7 +1,21 @@
-export default function Title(props: { children: string }) {
+import { ReactElement, ReactNode } from "react"
+
+export default function Title(props: { children: ReactNode }) {
+	const { children } = props
+
+	const canBeText = (child: ReactNode) => ["string", "number", "boolean"].includes(typeof child)
+
+	const getEl = (child: ReactNode): any => {
+		if (Array.isArray(child)) return child.map(getEl)
+		if (canBeText(child)) return <h2>{child}</h2>
+		const childEl = child as ReactElement
+		if (childEl.props.children.length > 1) return getEl(childEl.props.children)
+		return child
+	}
+
 	return (
-		<div className="border border-gray-400 border-t-0 border-x-0 pb-4 flex-none">
-			<h2>{props.children}</h2>
+		<div className="border border-gray-400 border-t-0 border-x-0 pb-4 flex-none flex flex-row justify-between">
+			{getEl(children)}
 		</div>
 	)
 }
